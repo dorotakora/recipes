d391b5352a3edb42fb031b4d573c516b5e04ec72
dorotakora <dorota.kora@o2.pl>
2020-01-15 11:16:09 +0100

    https

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import SearchForm from './SearchForm';
 import ForecastResult from './ForecastResult';
 import './App.css';
 
-const APIKey = '9161a3616ae201c026ff6383e35a0198';
+const APIKey = '837e046bf31e5cb351b5bf6ed80e01b6';
 
 class App extends Component {
 
@@ -15,8 +15,8 @@ class App extends Component {
     pressure: '',
     wind: '',
     err: false,
-   // conditionIcon: '',
-    //conditionText: '',
+    conditionIcon: '',
+    conditionText: '',
     //forecastArray: ''
   };
 
@@ -28,15 +28,7 @@ class App extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        let API;
-        if (window.location.protocol === 'http:') {
-
-          API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&units=metric&APPID=${APIKey}`; 
-        } else {
-
-          API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&units=metric&APPID=${APIKey}`; 
-        }
-        //const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&units=metric&APPID=${APIKey}`; 
+        const API = `http://api.weatherstack.com/current?access_key=${APIKey}&query=${this.state.value}`; //&days=7
 
         fetch(API)
             .then(response => {
@@ -47,17 +39,15 @@ class App extends Component {
             })
             .then(response => response.json())
             .then(data => {
-              console.log(data)
                 this.setState(prevState => ({
-                 
                     err: false,
-                    date: data.sys.sunrise,
-                    temp: data.main.temp,
-                    pressure: data.main.pressure,
-                    wind: data.wind.speed,
+                    date: data.location.localtime,
+                    temp: data.current.temperature,
+                    pressure: data.current.pressure,
+                    wind: data.current.wind_speed,
                     city: prevState.value,
-                    //conditionIcon: data.current.weather_icons,
-                    //conditionText: data.current.weather_descriptions,
+                    conditionIcon: data.current.weather_icons,
+                    conditionText: data.current.weather_descriptions,
                     // forecastArray: data.forecast.forecastday,
                     value: ''
                 }))
@@ -65,82 +55,7 @@ class App extends Component {
             })
             .catch(err => {
                 console.log(err);
-                this.setState(prevState => ({import React, { Component } from 'react';
-                import SearchForm from './SearchForm';
-                import ForecastResult from './ForecastResult';
-                import './App.css';
-                
-                const APIKey = '837e046bf31e5cb351b5bf6ed80e01b6';
-                
-                class App extends Component {
-                
-                  state = {
-                    value: '',
-                    date: '',
-                    city: '',
-                    temp: '',
-                    pressure: '',
-                    wind: '',
-                    err: false,
-                    conditionIcon: '',
-                    conditionText: '',
-                    //forecastArray: ''
-                  };
-                
-                  handleInputChange = e => {
-                    this.setState({
-                      value: e.target.value
-                    })
-                  };
-                
-                    handleSubmit = e => {
-                        e.preventDefault();
-                        const API = `http://api.weatherstack.com/current?access_key=${APIKey}&query=${this.state.value}`; //&days=7
-                
-                        fetch(API)
-                            .then(response => {
-                                if (response.ok) {
-                                    return response
-                                }
-                                throw Error("Wystąpił błąd")
-                            })
-                            .then(response => response.json())
-                            .then(data => {
-                                this.setState(prevState => ({
-                                    err: false,
-                                    date: data.location.localtime,
-                                    temp: data.current.temperature,
-                                    pressure: data.current.pressure,
-                                    wind: data.current.wind_speed,
-                                    city: prevState.value,
-                                    conditionIcon: data.current.weather_icons,
-                                    conditionText: data.current.weather_descriptions,
-                                    // forecastArray: data.forecast.forecastday,
-                                    value: ''
-                                }))
-                
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                this.setState(prevState => ({
-                                    err: true,
-                                    city: prevState.value
-                                }))
-                            })
-                    };
-                
-                  render() {
-                    return <div className="container">
-                            <SearchForm value={this.state.value}
-                                        change={this.handleInputChange}
-                                        submit={this.handleSubmit}/>
-                             <ForecastResult forecast={this.state} />
-                          </div>
-                  }
-                }
-                
-                export default App;
-                
+                this.setState(prevState => ({
                     err: true,
                     city: prevState.value
                 }))
